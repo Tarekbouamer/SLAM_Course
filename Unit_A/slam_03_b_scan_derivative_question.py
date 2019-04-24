9e37681251ed21ac5ed1932077f5dac47275c14d
tarek199147 <tarekbouamer1788@gmail.com>
2019-04-24 20:04:38 +0200

    //

--- Unit_A/slam_03_b_scan_derivative_question.py
+++ Unit_A/slam_03_b_scan_derivative_question.py
@@ -9,10 +9,13 @@ from pylab import *
 def compute_derivative(scan, min_dist):
     jumps = [ 0 ]
     for i in xrange(1, len(scan) - 1):
-        # --->>> Insert your code here.
-        # Compute derivative using formula "(f(i+1) - f(i-1)) / 2".
-        jump = 0.5 * (scan(i + 1) - scan(i - 1))
-        jumps.append(jump)  # Replace this line, append derivative instead.
+        head = scan[i + 1]
+        tail = scan[i - 1]
+        if (head > min_dist and tail > min_dist):
+            jump = 0.5 * (scan(i + 1) - scan(i - 1))
+            jumps.append(jump)
+        else:
+            jumps.append(0)
 
     jumps.append(0)
     return jumps
