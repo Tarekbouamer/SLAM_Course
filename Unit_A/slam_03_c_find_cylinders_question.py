131fae60eedfed43974df5bdd988665b101a8a32
tarek199147 <tarekbouamer1788@gmail.com>
2019-04-24 21:37:24 +0200

    //

--- Unit_A/slam_03_c_find_cylinders_question.py
+++ Unit_A/slam_03_c_find_cylinders_question.py
@@ -1,13 +1,14 @@
 # For each cylinder in the scan, find its ray and depth.
 # 03_c_find_cylinders
 # Claus Brenner, 09 NOV 2012
-from pylab import *
 from lego_robot import *
+from pylab import *
+
 
 # Find the derivative in scan data, ignoring invalid measurements.
 def compute_derivative(scan, min_dist):
     jumps = [ 0 ]
-    for i in xrange(1, len(scan) - 1):
+    for i in range(1, len(scan) - 1):
         l = scan[i-1]
         r = scan[i+1]
         if l > min_dist and r > min_dist:
@@ -21,19 +22,51 @@ def compute_derivative(scan, min_dist):
 # For each area between a left falling edge and a right rising edge,
 # determine the average ray number and the average depth.
 def find_cylinders(scan, scan_derivative, jump, min_dist):
+
     cylinder_list = []
     on_cylinder = False
-    sum_ray, sum_depth, rays = 0.0, 0.0, 0
+    direction = 'Left'
+    sum_ray = []
+    sum_depth = []
+    rays = 0.0
+    remove = False
 
-    for i in xrange(len(scan_derivative)):
-        # --->>> Insert your cylinder code here.
-        # Whenever you find a cylinder, add a tuple
-        # (average_ray, average_depth) to the cylinder_list.
+    for i in range(len(scan_derivative)):
 
-        # Just for fun, I'll output some cylinders.
-        # Replace this by your code.
-        if i % 100 == 0:
-            cylinder_list.append( (i, scan[i]) )
+        if abs(scan_derivative[i]) > jump:
+
+            if on_cylinder and direction == 'Left':
+
+                if scan_derivative[i] < 0:
+                    remove = True
+
+                else:
+                    on_cylinder = False
+                    average_ray = sum(sum_ray) / rays
+                    average_depth = sum(sum_depth) / rays
+                    cylinder_list.append((average_ray, average_depth))
+                    # delete
+                    sum_ray = []
+                    sum_depth = []
+                    rays = 0.0
+
+            if not on_cylinder and scan_derivative[i] < 0:
+                on_cylinder = True
+                direction = 'Left'
+
+        if scan[i] <= min_dist:
+            remove = True
+
+        if on_cylinder and scan[i] > min_dist:
+            rays += 1
+            sum_ray.append(i)
+            sum_depth.append(scan[i])
+
+        if remove:
+            sum_ray = []
+            sum_depth = []
+            rays = 0.0
+            remove = False
 
     return cylinder_list
 
@@ -52,6 +85,7 @@ if __name__ == '__main__':
 
     # Find cylinders.
     der = compute_derivative(scan, minimum_valid_distance)
+    print(der)
     cylinders = find_cylinders(scan, der, depth_jump,
                                minimum_valid_distance)
 
