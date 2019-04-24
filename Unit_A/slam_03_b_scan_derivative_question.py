3f450d59d2d306e61e1c6db7c446e121b4080782
tarek199147 <tarekbouamer1788@gmail.com>
2019-04-24 20:16:10 +0200

    //

--- Unit_A/slam_03_b_scan_derivative_question.py
+++ Unit_A/slam_03_b_scan_derivative_question.py
@@ -1,18 +1,19 @@
 # Compute the derivative of a scan.
 # 03_b_scan_derivative
 # Claus Brenner, 09 NOV 2012
-from lego_robot import *
-from pylab import *
+import pylab as plt
+
+import Unit_A.lego_robot as CL
 
 
 # Find the derivative in scan data, ignoring invalid measurements.
 def compute_derivative(scan, min_dist):
-    jumps = [ 0 ]
-    for i in xrange(1, len(scan) - 1):
+    jumps = [0]
+    for i in range(1, len(scan) - 1):
         head = scan[i + 1]
         tail = scan[i - 1]
         if (head > min_dist and tail > min_dist):
-            jump = 0.5 * (scan(i + 1) - scan(i - 1))
+            jump = 0.5 * (head - tail)
             jumps.append(jump)
         else:
             jumps.append(0)
@@ -26,19 +27,17 @@ if __name__ == '__main__':
     minimum_valid_distance = 20.0
 
     # Read the logfile which contains all scans.
-    logfile = LegoLogfile()
+    logfile = CL.LegoLogfile()
     logfile.read("robot4_scan.txt")
 
     # Pick one scan.
-    scan_no = 7
+    scan_no = 5
     scan = logfile.scan_data[scan_no]
-
     # Compute derivative, (-1, 0, 1) mask.
     der = compute_derivative(scan, minimum_valid_distance)
-    print(der)
 
     # Plot scan and derivative.
-    title("Plot of scan %d" % scan_no)
-    plot(scan)
-    plot(der)
-    show()
+    plt.title("Plot of scan %d" % scan_no)
+    plt.plot(scan)
+    plt.plot(der)
+    plt.show()
