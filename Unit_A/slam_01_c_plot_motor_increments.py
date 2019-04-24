7c525942507c63fc9206e7a564ed74d823a7afbc
tarek199147 <tarekbouamer1788@gmail.com>
2019-04-24 18:58:27 +0200

    A04

--- Unit_A/slam_01_c_plot_motor_increments.py
+++ Unit_A/slam_01_c_plot_motor_increments.py
@@ -1,7 +1,7 @@
 # Plot the increments of the left and right motor.
 # 01_c_plot_motor_increments.py
 # Claus Brenner, 07 NOV 2012
-from pylab import *
+import pylab  as plt
 from lego_robot import LegoLogfile
 
 if __name__ == '__main__':
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     logfile = LegoLogfile()
     logfile.read("robot4_motors.txt")
 
-    plot(logfile.motor_ticks)
-    show()
+    plt.plot(logfile.motor_ticks)
+    plt.show()
